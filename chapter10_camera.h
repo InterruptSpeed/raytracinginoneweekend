51d1f9ecff466bc1926ec4fa2b5c7c73b75610b0
Steven Docherty <sdocherty@InterruptSpeed.com>
2019-01-08 16:55:47 -0500

    added chapter 9-12

--- chapter10_camera.h
+++ chapter10_camera.h
@@ -12,8 +12,8 @@
 #ifndef CAMERAH
 #define CAMERAH
 #include "ray.h"
-
 #include "rand.h"
+
 // added to support M_PI
 #define _USE_MATH_DEFINES
 #include <math.h>
@@ -29,8 +29,8 @@ vec3 random_in_unit_disk() {
 
 class camera {
     public:
-        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist) { // vfov is top to bottom in degrees
-            lens_radius = aperture / 2;
+        camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect) { // vfov is top to bottom in degrees
+            vec3 u, v, w;
             float theta = vfov*M_PI/180;
             float half_height = tan(theta/2);
             float half_width = aspect * half_height;
@@ -38,22 +38,16 @@ class camera {
             w = unit_vector(lookfrom - lookat);
             u = unit_vector(cross(vup, w));
             v = cross(w, u);
-            lower_left_corner = origin  - half_width*focus_dist*u -half_height*focus_dist*v - focus_dist*w;
-            horizontal = 2*half_width*focus_dist*u;
-            vertical = 2*half_height*focus_dist*v;
+            lower_left_corner = vec3(-half_width, -half_height, -1.0);
+            horizontal = 2*half_width*u;
+            vertical = 2*half_height*v;
         }
-        ray get_ray(float s, float t) {
-            vec3 rd = lens_radius*random_in_unit_disk();
-            vec3 offset = u * rd.x() + v * rd.y();
-            return ray(origin + offset, lower_left_corner + s*horizontal + t*vertical - origin - offset); 
-        }
+        ray get_ray(float s, float t) { return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin); }
 
         vec3 origin;
         vec3 lower_left_corner;
         vec3 horizontal;
         vec3 vertical;
-        vec3 u, v, w;
-        float lens_radius;
 };
 #endif
 
